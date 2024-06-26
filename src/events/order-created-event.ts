f5cf102319c55a6fc71515075554f2a6de2e7d8e
Nathan Schrader <natecschrader@gmail.com>
2024-06-26 15:16:23 -0700

    Updates

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -5,6 +5,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     // expiresAt is a Date object in the order model
